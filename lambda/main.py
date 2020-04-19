2f0b138e0a76b808c0a6aa807e4cc74e5807fefd
exploding-pear <alexjdupper@gmail.com>
2020-04-19 18:08:22 -0400

    deserializing json

--- lambda/main.py
+++ lambda/main.py
@@ -5,9 +5,7 @@ def handler(event, context):
 	for key in event.keys():
 		print("{key}: {value}".format(key=key, value=event[key]))
 	
-	if not event['body']['status']:
-		message = json.loads('{ "message": "missing status" }')
-	
-	print(event['body']['status'])
+	body = json.dumps(event['body'])
+	print(body)
 
 	return(message)
