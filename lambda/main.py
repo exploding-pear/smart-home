5bb180c2fe53e4e175b1d291eb4feae4d2206733
exploding-pear <alexjdupper@gmail.com>
2020-04-19 18:21:35 -0400

    adding check for status type

--- lambda/main.py
+++ lambda/main.py
@@ -5,9 +5,19 @@ def handler(event, context):
 	for key in event.keys():
 		print("{key}: {value}".format(key=key, value=event[key]))
 	
-	message = json.loads('{"message": "hello world!"}')
+	if not event['body']:
+		message = json.loads('{"message": "missing status"}')
+		return(message)
 
 	body = json.loads(event['body'])
+	if body['status'] == "on":
+		message = json.loads('{"message": "switch on"}')
+	elif body['status'] == "off":
+		message = json.loads('{"message": "switch off"}')
+	else:
+		message = json.loads('{"message": "invalid status"}')
+	
+	return(message)
 
 	print(body['status'])
 
