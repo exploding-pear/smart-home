1a4922c6b555eedec70373cc5892a40c7a5a886a
exploding-pear <alexjdupper@gmail.com>
2020-04-19 18:02:26 -0400

    check for status in body

--- lambda/main.py
+++ lambda/main.py
@@ -4,4 +4,10 @@ import json
 def handler(event, context):
 	for key in event.keys():
 		print("{key}: {value}".format(key=key, value=event[key]))
-	return("{ \"message\": \"hello world!\"}")
+	
+	if not event['body']['status']:
+		message = json.loads('{ "message": "missing status" }')
+	
+	print(event['body']['status'])
+
+	return(message)
