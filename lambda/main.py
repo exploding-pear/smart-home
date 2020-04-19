776d610c37973c70a828c353bc1c1c3a017c5f55
exploding-pear <alexjdupper@gmail.com>
2020-04-19 18:09:18 -0400

    adding helloworld json response

--- lambda/main.py
+++ lambda/main.py
@@ -5,6 +5,8 @@ def handler(event, context):
 	for key in event.keys():
 		print("{key}: {value}".format(key=key, value=event[key]))
 	
+	message = json.loads('{"message": "hello world!"}')
+
 	body = json.dumps(event['body'])
 	print(body)
 
